9910ae528870debab684eaee434a8f2bf6db9642
zai-i <zainab@wonderbly.com>
2021-11-12 23:33:53 +0000

    landscape mode alert

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,9 @@ import Container from "../components/container";
 
 export default function Home() {
   return (
+    <>
+      <div className="alert"><p>⟳<br/><br/>Works best in landscape mode on larger screens</p></div>
      <Container/>
+    </>
   );
 }
