627599d7b0250da4bbcbfb3d72654fb0e2033a22
zai-i <zainab@wonderbly.com>
2021-11-13 17:17:37 +0000

    revert bug

--- src/components/drop.jsx
+++ src/components/drop.jsx
@@ -7,7 +7,6 @@ export default class Drop extends React.Component {
     }
 
    dropped = (e) => {
-        e.containerElem.style.visibility = 'hidden';
         e.target.style.color = '#111';
         e.target.style.webkitTextStroke = '5px #111'
     }; 
