c0cc1193b75df800fb12060434191ffe3e5c7f5a
zai-i <zainab@wonderbly.com>
2021-11-14 18:37:06 +0000

    title

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ import Container from "../components/container";
 export default function Home() {
   return (
     <>
-      <div className="alert"><p>Please put me in landscape mode</p><p>Turn your sound up to listen to the letters</p></div>
+      <div className="alert"><h1 className="title">ALIFBA</h1><h2 className="subtitle">drag and drop</h2><p>Please put me in landscape mode</p><p>Turn up your sound to listen to the letters</p></div>
      <Container/>
     </>
   );
