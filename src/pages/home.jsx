8addf1f1f706c1a6cc3b24cc1d7f1180f8706d34
zai-i <zainab@wonderbly.com>
2021-11-15 10:34:27 +0000

    splash screen

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ import Container from "../components/container";
 export default function Home() {
   return (
     <>
-      <div className="alert"><h1 className="title">ALIFBA</h1><h2 className="subtitle">drag and drop</h2><p>Please put me in landscape mode</p><p>Turn up the volume to listen to the letters</p></div>
+      <div className="alert"><h1 className="title"><span>A</span><span>L</span><span>I</span><span>F</span><span>B</span><span>A</span></h1><h2 className="subtitle">drag and drop</h2><p>Please put me in landscape mode and turn up the volume to listen to the letters</p></div>
      <Container/>
     </>
   );
