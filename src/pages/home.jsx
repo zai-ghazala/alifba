de6fd0ff56f1b374f20da10c171da5466c00ecf7
zai-i <zainab@wonderbly.com>
2022-12-18 14:36:15 +0000

    Orientation hook to stop overscroll

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,30 +1,14 @@
 import * as React from "react";
 import Container from "../components/container";
+import useScreenOrientation from 'react-hook-screen-orientation'
 
 export default function Home() {
 
-  const [orientation, setOrientation] = useState('portrait');
-
-useEffect(() => {
-    window.addEventListener('orientationchange', setScreenOrientation)
-    return () => {
-      document.removeEventListener('orientationchange', setScreenOrientation)
-    }
-  }, [])
-
-  const setScreenOrientation = () => {
-    if(window.matchMedia("(orientation: portrait)").matches){
-      setOrientation('portrait');
-    }
-
-    if(window.matchMedia("(orientation: landscape)").matches){
-      setOrientation('landscape');
-    }
-  }
+  const screenOrientation = useScreenOrientation()
 
   return (
     <>
-      {orientation === 'portrait' ? <div className="alert"><h1 className="title"><span>A</span><span>L</span><span>I</span><span>F</span><span>B</span><span>A</span></h1><h2 className="subtitle">DRAG & DROP</h2><p>Switch to landscape mode<br/>Turn up the volume</p></div> :
+      {screenOrientation === 'portrait-primary' || screenOrientation === 'portrait-secondary' ? <div className="alert"><h1 className="title"><span>A</span><span>L</span><span>I</span><span>F</span><span>B</span><span>A</span></h1><h2 className="subtitle">DRAG & DROP</h2><p>Switch to landscape mode<br/>Turn up the volume</p></div> :
      <Container/>}
     </>
   );
