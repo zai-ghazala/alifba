88aca5cbe83d1c65f241d79f83774129194a35f6
zai-i <zainab@wonderbly.com>
2022-12-18 14:39:03 +0000

    Revert

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,11 @@
 import * as React from "react";
 import Container from "../components/container";
-import useScreenOrientation from 'react-hook-screen-orientation'
 
 export default function Home() {
-
-  const screenOrientation = useScreenOrientation()
-
   return (
     <>
-      {screenOrientation === 'portrait-primary' || screenOrientation === 'portrait-secondary' ? <div className="alert"><h1 className="title"><span>A</span><span>L</span><span>I</span><span>F</span><span>B</span><span>A</span></h1><h2 className="subtitle">DRAG & DROP</h2><p>Switch to landscape mode<br/>Turn up the volume</p></div> :
-     <Container/>}
+      <div className="alert"><h1 className="title"><span>A</span><span>L</span><span>I</span><span>F</span><span>B</span><span>A</span></h1><h2 className="subtitle">DRAG & DROP</h2><p>Switch to landscape mode<br/>Turn up the volume</p></div>
+     <Container/>
     </>
   );
 }
