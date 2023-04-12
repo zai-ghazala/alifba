379a41a641503b9fdce05ebffc52a6698049b14a
zai-i <zainab@wonderbly.com>
2023-04-12 21:49:04 +0100

    Switch off silent mode

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ import Container from "../components/container";
 export default function Home() {
   return (
     <>
-      <div className="alert"><h1 className="title"><span>A</span><span>L</span><span>I</span><span>F</span><span>B</span><span>A</span></h1><h2 className="subtitle">DRAG & DROP</h2><p>Switch to landscape mode<br/>Turn up the volume</p></div>
+      <div className="alert"><h1 className="title"><span>A</span><span>L</span><span>I</span><span>F</span><span>B</span><span>A</span></h1><h2 className="subtitle">DRAG & DROP</h2><p>Switch to landscape mode<br/>Switch off silent mode<br/>Turn up the volume</p></div>
      <Container/>
     </>
   );
